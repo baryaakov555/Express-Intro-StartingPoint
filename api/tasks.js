28f5a823e6acd8b9b778a90fca500dc2eccf881d
Bar Yaakov <baryaakov5050@gmail.com>
2025-06-30 00:20:36 -0400

    Finished the backend

--- api/tasks.js
+++ api/tasks.js
@@ -4,16 +4,32 @@ const { Task } = require("../dummy-database");
 
 // GET all tasks
 router.get("/", (req, res) => {
-  // Replace this with your code!
-  res.status(501).send("Not implemented");
+  res.send(Task.findAll());
 });
 
 // GET a single task by id
+router.get("/:id", (req, res) => {
+  const taskId = Number(req.params.id);
+  res.send(Task.findByPk(taskId));
+});
 
 // Patch a task by id
+router.patch("/:id", (req, res) => {
+  const taskId = Number(req.params.id);
+  const task = req.body;
+  res.send(Task.update(taskId, task));
+});
 
 // Delete a task by id
+router.delete("/:id", (req, res) => {
+  const taskId = Number(req.params.id);
+  res.send(Task.delete(taskId));
+});
 
 // Create a new task
+router.post("/", (req, res) => {
+  const task = req.body;
+  res.send(Task.create(task))
+});
 
 module.exports = router;
